012be9f92bb8c7c0499f55d061341302e124771f
Jfaciane <john.faciane@gmail.com>
2020-05-03 18:57:52 +0000

    update server for mom site

--- server.py
+++ server.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def home():
     return send_from_directory('Website', 'index.html')
 
+@app.route("/vickis_vision/")
+def vicki_home():
+    return send_from_directory('vickis_vision', 'index.html')
+
+@app.route('/vickis_vision/<path:path>')
+def vicki_static_file(path):
+    print("Query path:", path)
+    return send_from_directory("vickis_vision", path)
 
 @app.route('/<path:path>')
 def static_file(path):
