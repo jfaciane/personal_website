f38f406ff461bbbd311f5784083ea83acdf9920e
Jfaciane <john.faciane@gmail.com>
2020-05-03 21:12:54 +0000

    turn off caching on server

--- server.py
+++ server.py
@@ -22,5 +22,17 @@ def static_file(path):
     print("Query path:", path)
     return send_from_directory("Website", path)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80)
